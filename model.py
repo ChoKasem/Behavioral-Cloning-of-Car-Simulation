1b4c404d14f24b6c40d50920538c819eafdf46ed
Chokpisit Kasemphaibulsuk <chokpisit.kasemphaibulsuk@mail.utoronto.ca>
2020-06-18 10:29:13 -0400

    create and modify model.py

---
+++ model.py
@@ -0,0 +1,42 @@
+from keras.models import Sequential, Model
+from keras.layers import Cropping2D, Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPool2D
+import numpy as np
+import cv2
+
+import csv
+
+lines = []
+with open('../data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = '../data/IMG/' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+# set up cropping2D layer
+model = Sequential()
+model.add(Lambda(lambda x: x/255.0-0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPool2D())
+model.add(Convolution2D(6,5,5,activation="relu"))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+
+model.save('model.h5')
+exit()
